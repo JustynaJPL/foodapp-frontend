1ca674aa2c7e6951f1edf0f5b079b82b971316e3
Kyonea <cooperation.justa.stobiecka@gmail.com>
2024-01-02 23:24:35 +0100

    work for 02.01.2024 - fixed nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoggerModule } from './logger/logger.module';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, NavigationComponent],
+  imports: [CommonModule, RouterModule, RouterOutlet, FooterComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  private collapsenavi = false;
+
+  public toggleNaviMenu():void{
+    this.collapsenavi = !this.collapsenavi;
+  }
+
+  public gettogglenavi():boolean{
+    return this.collapsenavi;
+  }
 
 }
